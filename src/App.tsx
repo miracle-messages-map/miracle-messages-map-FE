d82793383eec2042c18f6e909b21b5483a40f451
Miikis <28997062+Miikis@users.noreply.github.com>
2019-06-25 20:41:09 -0700

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from "react"
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom"
+
+const VolunteerRegistration = () => {
+    return (
+        <>
+            <h2>Register</h2>
+            <p>Apply to be a volunteer</p>
+        </>
+    )
+}
+const Login = () => {
+    return (
+        <>
+            <h2>Login</h2>
+            <p>Login to your account</p>
+        </>
+    )
+}
 
 const App: React.FC = () => {
     return (
-        <>
-            <h1>Miracle Messages</h1>
-        </>
+        <Router>
+            <header>
+                <h1>Miracle Messages</h1>
+                <nav>
+                    <NavLink to="/apply">Become a Volunteer</NavLink>
+                    <NavLink to="/login">Login</NavLink>
+                </nav>
+            </header>
+            <Route exact path="/" component={VolunteerRegistration} />
+            <Route path="/apply" component={VolunteerRegistration} />
+            <Route path="/login" component={Login} />
+        </Router>
     )
 }
 
