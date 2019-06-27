521d1bf922a6251e8a18f7eda71927967e97aa2d
Miikis <28997062+Miikis@users.noreply.github.com>
2019-06-27 14:23:08 -0700

    Add navbar with logout button

--- src/components/Layout/AuthorizedLayout.js
+++ src/components/Layout/AuthorizedLayout.js
@@ -1,7 +1,48 @@
 import React from "react"
+import { Nav, Navbar, NavDropdown } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
-const AuthorizedLayout = ({ children }) => {
-    return <div>{children}</div>
+import logo from "../../images/logo.png"
+
+const NavbarLogo = () => {
+    return (
+        <Link to="/dashboard" className="topnav-logo">
+            <img src={logo} alt="Logo of Miracle Messages" height="32" />
+        </Link>
+    )
+}
+
+const DashboardNavbar = ({ history }) => {
+    const handleLogout = () => {
+        localStorage.clear()
+        history.push("/login")
+    }
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand>
+                <NavbarLogo />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse style={{ "justify-content": "flex-end" }}>
+                <Nav className="">
+                    <NavDropdown title="BarnBarn" className="float-right mb-0">
+                        <NavDropdown.Item onClick={handleLogout}>
+                            Logout
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+const AuthorizedLayout = ({ children, history }) => {
+    return (
+        <>
+            <DashboardNavbar history={history} />
+            {children}
+        </>
+    )
 }
 
 export default AuthorizedLayout
